37de67d28fc8f2d0a08e6a21cb31855ba5b3ca5e
Asa Ayers <Asa@AsaAyers.com>
2017-11-24 07:25:34 -0800

    Separate mapping from traversal

--- src/index.js
+++ src/index.js
@@ -6,28 +6,49 @@ import type {
 } from 'graphql'
 import {
   parse,
-  visit
+  visit,
+  getVisitFn
 } from 'graphql'
+
+const mapToNewTree = (map, visitor) => {
+  const get = map.get.bind(map)
+  function replaceNode (node, key, parent, path, ancestors, leaving) {
+    var visitFn = getVisitFn(visitor, node.kind, leaving)
+    if (visitFn) {
+      const newNode = visitFn.call(visitor, { node, key, parent, path, ancestors, get })
+      if (newNode != null) {
+        map.set(node, newNode)
+      }
+    }
+  }
+
+  return {
+    enter (node, key, parent, path, ancestors) {
+      replaceNode(node, key, parent, path, ancestors, false)
+    },
+    leave (node, key, parent, path, ancestors) {
+      replaceNode(node, key, parent, path, ancestors, true)
+    }
+  }
+}
 
 export function transform (schemaText: string): * {
   const graphqlAst = parse(schemaText)
   type BabelNode = mixed
-  const map: WeakMap<GQLNode, BabelNode> = new WeakMap()
 
   const visitors = {
     Document: {
-      leave (node) {
-        console.log(node)
-        const body = node.definitions.map(map.get.bind(map))
+      leave ({ get, node }) {
+        const body = node.definitions.map(get)
         const directives = []
 
-        map.set(node, t.program(
+        return t.program(
           body, directives, 'module'
-        ))
+        )
       }
     },
     NamedType: {
-      leave (node, key, parent) {
+      leave ({ get, node, key, parent }) {
         const { value } = node.name
 
         let identifier = t.identifier(node.name.value)
@@ -39,22 +60,19 @@ export function transform (schemaText: string): * {
           identifier = t.nullableTypeAnnotation(identifier)
         }
 
-        map.set(node, identifier)
+        return identifier
       }
     },
     NonNullType: {
-      leave (node) {
-        map.set(node,
-          map.get(node.type)
-        )
-        return false
+      leave ({ get, node }) {
+        return get(node.type)
       }
     },
     InputValueDefinition: {
-      leave (node, parent) {
+      leave ({ get, node, parent }) {
         const key = t.identifier(node.name.value)
 
-        const value = map.get(node.type)
+        const value = get(node.type)
 
         const otp = t.objectTypeProperty(key, value)
         otp.kind = 'init'
@@ -63,30 +81,28 @@ export function transform (schemaText: string): * {
         otp.static = false
         otp.variance = null
 
-        map.set(node, otp)
+        return otp
       }
     },
     InputObjectTypeDefinition: {
-      leave (node) {
+      leave ({ get, node }) {
         const id = t.identifier(node.name.value)
         const typeParameters = null
 
-        const properties = node.fields.map(map.get.bind(map))
+        const properties = node.fields.map(get)
         const indexers = []
         const callProperties = []
 
         const right = t.objectTypeAnnotation(properties, indexers, callProperties)
         right.exact = true
 
-        map.set(node, t.typeAlias(id, typeParameters, right))
-        // const NAME = node.name.value
-        // const PARAMS = context.params
-        // console.log('node', tmpl({ NAME, PARAMS }))
+        return t.typeAlias(id, typeParameters, right)
       }
     }
   }
 
-  visit(graphqlAst, visitors)
+  const map: WeakMap<GQLNode, BabelNode> = new WeakMap()
+  visit(graphqlAst, mapToNewTree(map, visitors))
 
   return map.get(graphqlAst)
 }
